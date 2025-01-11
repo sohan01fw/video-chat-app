3f1091f3fd570ffff03d538f803311abd0faa57c
sohan01fw <sohancwactfw@gmail.com>
2025-01-11 21:33:05 +0545

    fix url placement

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -12,7 +12,7 @@ export const SocketProvider = ({ children }) => {
   const prod_url = import.meta.env.VITE_SERVER_WEBSOCKET_PRODUCTION_URL;
   const local_url = import.meta.env.VITE_SERVER_LOCAL_URL;
   const socketio = useMemo(
-    () => io(local_url ? local_url : prod_url),
+    () => io(local_url || prod_url),
     [prod_url, local_url],
   );
   return <Context.Provider value={socketio}>{children}</Context.Provider>;
