0c9f74ec1b46c51be4130116234d84d148a1b840
sohan01fw <sohancwactfw@gmail.com>
2025-01-11 20:57:31 +0545

    add start script file

--- server/lib/redis/redis_core.js
+++ server/lib/redis/redis_core.js
@@ -1,3 +1,5 @@
 import Redis from "ioredis";
 
-export const client = Redis.createClient();
+export const client = new Redis(
+  `rediss://default:${process.env.UPSTASH_REDIS_REST_TOKEN}@${process.env.UPSTASH_REDIS_REST_URL}:6379`,
+);
