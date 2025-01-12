49126e96c35c8efcfe54cee0dc25a88b58e7c058
sohan01fw <sohancwactfw@gmail.com>
2025-01-12 12:07:45 +0545

    check for the prod to set host url

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,11 +9,17 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = ({ children }) => {
+  // Check environment mode (production or development)
   const prod_url = import.meta.env.VITE_SERVER_WEBSOCKET_PRODUCTION_URL;
   const local_url = import.meta.env.VITE_SERVER_LOCAL_URL;
-  const socketio = useMemo(
-    () => io(local_url || prod_url),
-    [prod_url, local_url],
-  );
+
+  // Use correct WebSocket URL based on environment
+  const socketio = useMemo(() => {
+    if (import.meta.env.MODE === "production") {
+      return io(prod_url); // Use production URL
+    }
+    return io(local_url || prod_url); // Use local URL or fallback to prod URL if available
+  }, [prod_url, local_url]);
+
   return <Context.Provider value={socketio}>{children}</Context.Provider>;
 };
